0a94be87584511ca02d64471122a962c38fc4225
nikita_romanovich1@bat.com <nikita_romanovich1@bat.com>
2023-02-22 16:24:50 +0300

    added all models

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { sequelize } from './db.js';
+import models from './models/models.js'
+import cors from 'cors'
 import * as dotenv from 'dotenv'
+import { json } from 'sequelize';
 dotenv.config()
 
 const PORT = process.env.PORT;
 
 const app = express();
+app.use(cors())
+app.use(express.json())
 
 const start = async () => {
     try {
