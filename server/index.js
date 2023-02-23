dae4efd24a7f8845cc724c0dbc32a973474a5ef6
nikita_romanovich1@bat.com <nikita_romanovich1@bat.com>
2023-02-23 17:45:29 +0300

    added logic to deviceController

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import path from 'path';
 import router from './src/routes/Router.js'
 import fileUpload from 'express-fileupload';
 import { sequelize } from './db.js';
@@ -14,6 +15,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve('src', 'static')))
 app.use(fileUpload({}));
 app.use('/api', router);
 app.use(errorHandler);
--- server/src/controllers/deviceController.js
+++ server/src/controllers/deviceController.js
@@ -1,6 +1,6 @@
 import * as uuid from 'uuid';
 import path from 'path';
-import { Device } from '../models/models.js'
+import { Device, DeviceInfo } from '../models/models.js'
 import ApiError from '../errors/ApiError.js'
 
 class DeviceController { 
@@ -10,8 +10,19 @@ class DeviceController {
             const {img} = req.files;
             let filename = uuid.v4() + '.jpg';
             img.mv(path.resolve('src', 'static', filename));
-    
-            const device = await Device.create({name, price, brandId, typeId, img: filename})
+            const device = await Device.create({name, price, brandId, typeId, img: filename});
+
+            if(info) {
+                info = JSON.parse(info);
+                info.forEach(info => {
+                    DeviceInfo.create({
+                        title: info.title,
+                        description: info.description,
+                        deviceId: device.id
+                    })
+                });
+            }
+
             return res.json(device);
 
         } catch (e) {
@@ -21,11 +32,27 @@ class DeviceController {
     }
 
     async getAll(req, res) {
+        let {brandId, typeId, limit, page, info} = req.query;
+        page = page || 1;
+        limit = limit || 10;
+        let offset = page * limit - limit;
+        let devices;
 
+        !brandId && !typeId ? devices = await Device.findAndCountAll({limit, offset}) :
+        brandId && !typeId ?  devices = await Device.findAndCountAll({where: {brandId, limit, offset}}) :
+        !brandId && typeId ? devices = await Device.findAndCountAll({where: {typeId, limit, offset}}) :
+        brandId && typeId ? devices = await Device.findAndCountAll({where: {typeId, brandId, limit, offset}}) : null;
+
+        res.json(devices)
     }
 
     async getOne(req, res) {
-
+        const {id} = req.params;
+        const device = await Device.findOne({
+            where: {id},
+            include: [{model: DeviceInfo, as: 'info'}]
+        });
+        return res.json(device);
     }
 
 }
